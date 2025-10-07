44ddaee55b4d0f44fc770352d4c0dae272e21569
HermanSterman <harmen30@gmail.com>
2025-10-07 16:18:52 +0200

    update

--- Memory.java
+++ Memory.java
@@ -13,7 +13,7 @@ class GFG {
         JFrame frame = new JFrame();
 
         // Creating instance of JButton
-        JButton button = new JButton("Lowijs");
+        JButton button = new JButton("Nu wel runnen");
 
         // x axis, y axis, width, height
         button.setBounds(150, 200, 220, 50);
@@ -30,5 +30,4 @@ class GFG {
         // making the frame visible
         frame.setVisible(true);
     }
-
 }
